3054bd0196d1ef5472cb34c180e32b234dca5e32
syamn <admin@sakura-server.net>
2013-04-09 22:46:51 +0900

    Fix spaces, minor issue.

--- app/Controller/RevisionsController.php
+++ app/Controller/RevisionsController.php
@@ -68,7 +68,7 @@ class RevisionsController extends AppController {
 		// End of github settings
 
 		// begen new logfile
-    	$this->_log("==BEGEN REQUEST==", true);
+		$this->_log("==BEGEN REQUEST==", true);
 
 		// check from github IP
 		if (!in_array($_SERVER['REMOTE_ADDR'], $GITHUB_IPS)){
@@ -79,65 +79,65 @@ class RevisionsController extends AppController {
 
 		// check req data
 		if (!isset($_POST['payload'])){
-	        $this->_log("Error: Request error (POST['payload'])");
-	        exit;
-    	}
+			$this->_log("Error: Request error (POST['payload'])");
+			exit;
+		}
 
-    	$payload_raw = $_POST['payload'];
-    	try{
-	        $payload = json_decode($payload_raw); // json_decode(stripslashes($payload_raw));
-	    }catch(Exception $ex){
-	        $this->_log("Error: Occred exception while json_decode!");
-	        $this->_log(print_r($ex, TRUE));
-	        exit;
-	    }
+		$payload_raw = $_POST['payload'];
+		try{
+			$payload = json_decode($payload_raw); // json_decode(stripslashes($payload_raw));
+		}catch(Exception $ex){
+			$this->_log("Error: Occred exception while json_decode!");
+			$this->_log(print_r($ex, TRUE));
+			exit;
+		}
 
-	    if (!is_object($payload) || !is_array($payload->commits)){
-	        $this->_log("Error: Empty or no commits payload!");
-	        exit;
-	    }
+		if (!is_object($payload) || !is_array($payload->commits)){
+			$this->_log("Error: Empty or no commits payload!");
+			exit;
+		}
 
-	    $this->_log("Payload Contents: ");
-    	$this->_log(print_r($payload, TRUE));
+		$this->_log("Payload Contents: ");
+		$this->_log(print_r($payload, TRUE));
 
-    	// get detail
-	    /* Pusher */
-	    $pusher_name = $payload->pusher->name;
-	    $pusher_email = $payload->pusher->email;
+		// get detail
+		/* Pusher */
+		$pusher_name = $payload->pusher->name;
+		$pusher_email = $payload->pusher->email;
 
-	    /* Repository */
-	    $repo_name = $payload->repository->name;
-	    $repo_url = $payload->repository->url;
-	    $repo_owner = $payload->repository->owner->name;
+		/* Repository */
+		$repo_name = $payload->repository->name;
+		$repo_url = $payload->repository->url;
+		$repo_owner = $payload->repository->owner->name;
 
-	    /* Etc */
-	    $ref = $payload->ref; // check pushed to master? if(=== 'refs/heads/master'){}
-	    $compare_url = $payload->compare;
+		/* Etc */
+		$ref = $payload->ref; // check pushed to master? if(=== 'refs/heads/master'){}
+		$compare_url = $payload->compare;
 
-	    // Check repo owner
-        if (!in_array($repo_owner, $ALLOW_REPO_OWNER)){
-            $this->_log("Error: This repo owner '".$repo_owner."' not contains to allows list");
-            exit;
-        }
+		// Check repo owner
+		if (!in_array($repo_owner, $ALLOW_REPO_OWNER)){
+			$this->_log("Error: This repo owner '".$repo_owner."' not contains to allows list");
+			exit;
+		}
 
-	    $now = time();
-	    $repoId = $payload->repository->id;
+		$now = time();
+		$repoId = $payload->repository->id;
 
-	    $this->_log("Starting foreach loop");
-    	foreach($payload->commits as $commit) {
-    		/* Commits */
-	        $commit_url = $commit->url;
-	        $commit_msg = $commit->message;
-	        $committer_name = $commit->author->name;
-	        $committer_username = $commit->author->username;
-	        $committer_email = $commit->author->email;
-	        $hash = $commit->id;
-	        $shortHash = mb_substr($hash, 0, 6);
+		$this->_log("Starting foreach loop");
+		foreach($payload->commits as $commit) {
+			/* Commits */
+			$commit_url = $commit->url;
+			$commit_msg = $commit->message;
+			$committer_name = $commit->author->name;
+			$committer_username = $commit->author->username;
+			$committer_email = $commit->author->email;
+			$hash = $commit->id;
+			$shortHash = mb_substr($hash, 0, 6);
 
-	        $this->_log("Updating database for commit sha ".$shortHash);
+			$this->_log("Updating database for commit sha ".$shortHash);
 
-	        // Update Database
-	        $newRaw = array('CommitLog' => array(
+			// Update Database
+			$newRaw = array('CommitLog' => array(
 				'repo_id' => $repoId,
 				'hash' => $shortHash,
 				'author' => $committer_username,
@@ -146,38 +146,38 @@ class RevisionsController extends AppController {
 			));
 			$this->CommitLog->create();
 			$this->CommitLog->save($newRaw);
-    	}
+		}
 
-    	// Update twitter status
-    	$this->_log("Building update message");
+		// Update twitter status
+		$this->_log("Building update message");
 
-    	$tweet = null;
-    	foreach ($repoList as $urlName => $data){
-    		if ($data[0] == (int)$repoId){
+		$tweet = null;
+		foreach ($this->repoList as $urlName => $data){
+			if ($data[0] == (int)$repoId){
 				$tweet = $committer_username.'が'.$data[1].'を更新しました: http://sakurapvp.net/revisions/'.$urlName.' (HEAD: '.$shortHash.')';
-    		}
-    	}
-    	if (!empty($tweet)){
-    		if (mb_strlen($tweet) > 140){
-		        $tweet = mb_substr($tweet, 0, 138)."..";
-		    }
-		    $this->_log("Tweeting..: ".$tweet);
-		    $twitter = new OAuthClient(OAUTH_CONFIG::CONSUMER_KEY, OAUTH_CONFIG::CONSUMER_SECRET);
+			}
+		}
+		if (!empty($tweet)){
+			if (mb_strlen($tweet) > 140){
+				$tweet = mb_substr($tweet, 0, 138)."..";
+			}
+			$this->_log("Tweeting..: ".$tweet);
+			$twitter = new OAuthClient(OAUTH_CONFIG::CONSUMER_KEY, OAUTH_CONFIG::CONSUMER_SECRET);
 			$twitter->post(OAUTH_CONFIG::ACCESS_TOKEN, OAUTH_CONFIG::ACCESS_TOKEN_SECRET, 
 				'https://api.twitter.com/1/statuses/update.json', array('status' => $tweet));
-    	}else{
-    		$this->_log("Tweet message is empty! Skipped tweeting!");
-    	}
+		}else{
+			$this->_log("Tweet message is empty! Skipped tweeting!");
+		}
 
-    	// end of logfile
-    	$this->_log("==END==");
+		// end of logfile
+		$this->_log("==END==");
 	}
 
 	private function _log($line, $newFile = false){
-        if ($newFile){
-            file_put_contents('log/last_github_hook.log', $line."\n");
-        }else{
-            file_put_contents('log/last_github_hook.log', $line."\n", FILE_APPEND);
-        }
-    }
+		if ($newFile){
+			file_put_contents('log/last_github_hook.log', $line."\n");
+		}else{
+			file_put_contents('log/last_github_hook.log', $line."\n", FILE_APPEND);
+		}
+	}
 }
